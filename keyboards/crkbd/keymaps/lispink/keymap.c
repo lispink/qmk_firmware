1ccd8d27f1bd3908e62eff6d8e706d0234a8a064
Robin Goemaat <lispink000@gmail.com>
2022-04-14 00:38:29 +0200

    updated special character layer refactored gamemode

--- keyboards/crkbd/keymaps/lispink/keymap.c
+++ keyboards/crkbd/keymaps/lispink/keymap.c
@@ -19,13 +19,15 @@ along with this program.  If not,    see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
-enum crkbd_layers { _QWERTY, _QWERTYG, _NUMS, _NUMSG, _SPEC, _ADJUST };
+enum crkbd_layers { _QWERTY, _NUMS, _SPEC, _ADJUST };
 
-enum crkbd_keycodes { DT_GAME = SAFE_RANGE };
+enum crkbd_keycodes {
+    DT_GAME = SAFE_RANGE, // switch on game mode
+    ESC_SFT               // Esc in normal mode left shift in game mode
+};
 
 #define C_A_DEL LALT(LCTL(KC_DEL))
 #define NUMS MO(_NUMS)
-#define NUMSG MO(_NUMSG)
 #define SPEC MO(_SPEC)
 
 // clang-format off
@@ -34,53 +36,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
          KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-         KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+        ESC_SFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                             KC_LALT,    NUMS,  KC_SPC,    KC_RSFT,    SPEC, KC_RGUI
                                         //`--------------------------'  `--------------------------'
     ),
-    [_QWERTYG] = LAYOUT_split_3x6_3(
-    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
-    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_LALT,   NUMSG,  KC_SPC,    KC_RSFT,    SPEC, KC_RGUI
-                                        //`--------------------------'  `--------------------------'
-    ),
     [_NUMS] = LAYOUT_split_3x6_3(
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
+         KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-         KC_DEL, _______, _______, _______, _______, _______,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, _______,
+         KC_DEL, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, _______,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        _______, _______, _______, _______, _______, _______,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, _______,
+        _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                             _______, _______, _______,    _______, _______, _______
                                         //`--------------------------'  `--------------------------'
     ),
-    [_NUMSG] = LAYOUT_split_3x6_3(
-    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-         KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,   KC_P7,   KC_P8,   KC_P9,    KC_0, KC_NLCK,
-    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-         KC_DEL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      XXXXXXX,   KC_P4,   KC_P5,   KC_P6, XXXXXXX, XXXXXXX,
-    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-         KC_GRV, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC_P1,   KC_P2,   KC_P3, XXXXXXX, XXXXXXX,
-    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            _______, _______, _______,    _______, _______,   KC_P0
-                                        //`--------------------------'  `--------------------------'
-    ),
     [_SPEC] = LAYOUT_split_3x6_3(
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-         KC_GRV, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+         KC_GRV, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS,  KC_EQL, _______,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-         KC_DEL, KC_LPRN, KC_LBRC, KC_RBRC, KC_RPRN, KC_QUOT,                      KC_MINS,  KC_EQL, KC_BSLS, KC_LBRC, KC_RBRC,  KC_GRV,
+         KC_DEL, KC_LPRN, KC_LCBR, KC_LBRC, KC_LABK, KC_MINS,                      KC_PLUS, KC_RABK, KC_RBRC, KC_RCBR, KC_RPRN,  KC_GRV,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        _______,   KC_LT, KC_LCBR, KC_RCBR,   KC_GT, KC_DQUO,                      KC_UNDS, KC_PLUS, KC_PIPE, KC_LCBR, KC_RCBR, KC_TILD,
+        _______, KC_PIPE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_LABK, KC_RABK, KC_BSLS, _______,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                             _______, _______, _______,    _______, _______, _______
                                         //`--------------------------'  `--------------------------'
@@ -100,25 +80,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    if (default_layer_state == 1UL << _QWERTYG) {
-        return update_tri_layer_state(state, _SPEC, _NUMSG, _ADJUST);
-    } else {
-        return update_tri_layer_state(state, _SPEC, _NUMS, _ADJUST);
-    }
+    return update_tri_layer_state(state, _SPEC, _NUMS, _ADJUST);
 }
+
+bool gamemode = false; // used to swap a couple keys for games
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case DT_GAME:
             if (record->event.pressed) {
-                if (default_layer_state == 1UL << _QWERTYG) {
-                    set_single_persistent_default_layer(_QWERTY);
+                if (gamemode) {
+                    gamemode = false;
                 } else {
-                    set_single_persistent_default_layer(_QWERTYG);
+                    gamemode = true;
                 }
             }
             return false;
-            break;
+        case ESC_SFT:
+            if (record->event.pressed) {
+                if (gamemode) {
+                    register_code(KC_LSFT);
+                } else {
+                    register_code(KC_ESC);
+                }
+            } else {
+                if (gamemode) {
+                    unregister_code(KC_LSFT);
+                } else {
+                    unregister_code(KC_ESC);
+                }
+            }
+            return false;
+    }
+    // disable homerow keys on number layer in gamemode to prevent unintended keypresses while using WASD and hitting a number
+    if (gamemode && IS_LAYER_ON(_NUMS) && record->event.key.row == 1 && record->event.key.col > 0 && record->event.key.col < 6) {
+        return false;
     }
     return true;
 }
