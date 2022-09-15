d5665e24a0e157955b0673465352028f68190a1c
Robin Goemaat <lispink000@gmail.com>
2022-09-15 22:38:19 +0200

    update keymap

--- keyboards/crkbd/keymaps/lispink/keymap.c
+++ keyboards/crkbd/keymaps/lispink/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_SPEC] = LAYOUT_split_3x6_3(
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-         KC_GRV, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS,  KC_EQL, _______,
+        KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS,  KC_EQL, _______,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
          KC_DEL, KC_LPRN, KC_LCBR, KC_LBRC, KC_LABK, KC_MINS,                      KC_PLUS, KC_RABK, KC_RBRC, KC_RCBR, KC_RPRN,  KC_GRV,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
