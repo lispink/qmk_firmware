fc936ba3c4c1f2e08b731df7d52b032b38ca5301
Robin Goemaat <lispink000@gmail.com>
2022-10-10 21:11:29 +0200

    updated lispink crkbd keymap

--- keyboards/crkbd/keymaps/lispink/keymap.c
+++ keyboards/crkbd/keymaps/lispink/keymap.c
@@ -21,11 +21,6 @@ along with this program.  If not,    see <http://www.gnu.org/licenses/>.
 
 enum crkbd_layers { _QWERTY, _NUMS, _SPEC, _ADJUST };
 
-enum crkbd_keycodes {
-    DT_GAME = SAFE_RANGE, // switch on game mode
-    ESC_SFT               // Esc in normal mode left shift in game mode
-};
-
 #define C_A_DEL LALT(LCTL(KC_DEL))
 #define NUMS MO(_NUMS)
 #define SPEC MO(_SPEC)
@@ -36,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
          KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        ESC_SFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+         KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
     //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -67,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_ADJUST] = LAYOUT_split_3x6_3(
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        C_A_DEL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, DT_GAME,
+        C_A_DEL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_LSFT, RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG,  KC_F11,                       KC_F12, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU, KC_PSCR,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -81,40 +76,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _SPEC, _NUMS, _ADJUST);
-}
-
-bool gamemode = false; // used to swap a couple keys for games
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case DT_GAME:
-            if (record->event.pressed) {
-                if (gamemode) {
-                    gamemode = false;
-                } else {
-                    gamemode = true;
-                }
-            }
-            return false;
-        case ESC_SFT:
-            if (record->event.pressed) {
-                if (gamemode) {
-                    register_code(KC_LSFT);
-                } else {
-                    register_code(KC_ESC);
-                }
-            } else {
-                if (gamemode) {
-                    unregister_code(KC_LSFT);
-                } else {
-                    unregister_code(KC_ESC);
-                }
-            }
-            return false;
-    }
-    // disable homerow keys on number layer in gamemode to prevent unintended keypresses while using WASD and hitting a number
-    if (gamemode && IS_LAYER_ON(_NUMS) && record->event.key.row == 1 && record->event.key.col > 0 && record->event.key.col < 6) {
-        return false;
-    }
-    return true;
 }
